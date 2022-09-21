1854e29a510fa61a16d111017dac3a7b722fcc64
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-09-21 18:00:48 -0500

    added template literal to create readme when user inputs data

--- Develop/index.js
+++ Develop/index.js
@@ -1,78 +1,107 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const createREADme = require('/.generateMarkdown.js')
+// Begin Readme generator and data input 
+const generateReadme = (data) =>
+`
+# ${data.title}
+## Description
+${data.description}
+
+## Table of Contents
+[Installation](#installation)
+
+[Usage](#usage)
+
+[License](#license)
+
+[Contribution](#contribution)
+
+[Test Instructions](#testinstructions)
+
+[Questions](#github)
+
+## Installation
+${data.installation}
+    
+## Usage
+${data.usage}
+    
+## License
+Check out the badges hosted by [shields.io](https://shields.io/). [![License](https://img.shields.io/badge/License-${data.license}-blue.svg)](https://opensource.org/licenses/${data.license})
+    
+## Contributions
+${data.contribution}
+    
+## Test Instructions
+${data.testinstructions}
+
+## Questions 
+If you have any questions, reach out to me at:
+${data.github}
+${data.email}
+`
 
 // TODO: Create an array of questions for user input
-const questions = [
-    inquirer
+
+inquirer
     .prompt([
-      {
-        type: 'input',
-        message: 'What is the title of your project?',
-        name: 'title',
-      },
-      {
-          type: 'input',
-          message: 'Write a description about your project',
-          name: 'description',
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'title',
         },
         {
-          type: 'input',
-          message: 'What are the instructions of installation of your project?',
-          name: 'instructions',
+            type: 'input',
+            message: 'Write a description about your project',
+            name: 'description',
         },
         {
-          type: 'input',
-          message: 'What is the purpose of your project?',
-          name: 'usage',
+            type: 'input',
+            message: 'What are the instructions of installation of your project?',
+            name: 'installation',
         },
         {
-          type: 'input',
-          message: 'Write the contribution guidelines for your project',
-          name: 'contributuion',
+            type: 'input',
+            message: 'What is the purpose of your project?',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Write the contribution guidelines for your project',
+            name: 'contribution',
         },
         {
             type: 'input',
             message: 'What are the test instructions for your project?',
-            name: 'test',
-          },
-          {
+            name: 'testinstructions',
+        },
+        {
             type: 'input',
             message: 'What is your Github Username?',
             name: 'github',
-          },
-          {
+        },
+        {
             type: 'input',
             message: 'What is your email?',
             name: 'email',
-          },
-          {
-            type: 'input',
+        },
+        {
+            type: 'list',
             message: 'Which license would you like to use for your project?',
             name: 'license',
-          },
-    ]) 
-    // 'What is the title of your project?',
-    // 'Write a description about your project.',
-    // 'What are the instructions of installation of your project?',
-    // 'What is the purpose of your project?',
-    // 'Write the contribution guidlines for your project.',
-    // 'What are the test instructions for your project?',
-    // 'What is your GitHub Username?',
-    // 'What is your email?',
-    // 'Which license would you like to use for your project?'
-];
+            choices: ['MIT', 'Apache_2.0', 'Boost_1.0'],
+        },
+    ])
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile('README.md', JSON.stringify(data, null, 2), (err) =>
-    err ? console.error(err) : console.log('Success!')
-    )
-}
 
-// TODO: Create a function to initialize app
-function init() {}
+    // TODO: Create a function to write README file
+    // Grab the responses and generate the README with the responses.
 
-// Function call to initialize app
-init();
+
+    .then((response) => {
+        const readmeContent = generateReadme(response)
+        fs.writeFile('README.md', readmeContent, (err) =>
+            err ? console.error(err) : console.log('Success!')
+        )
+    })
