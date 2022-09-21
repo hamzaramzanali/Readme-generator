9cafeff610ff2f6ff045bb4c2a2d99e4d534c36d
Hamza Ramzanali <hamzaramzanali@Hamzas-MacBook-Air.local>
2022-09-21 15:08:39 -0500

    added array of questions and packages to index.js

---
+++ Develop/index.js
@@ -0,0 +1,25 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs')
+
+// TODO: Create an array of questions for user input
+const questions = [
+    'What is the title of your project?',
+    'Write a description about your project.',
+    'What are the instructions of installation of your project?',
+    'What is the purpose of your project?',
+    'Write the contribution guidlines for your project.',
+    'What are the test instructions for your project?',
+    'What is your GitHub Username?',
+    'What is your email?',
+    'Which license would you like to use for your project?'
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
